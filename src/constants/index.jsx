b6ceabec9fc0048fca9a14d9cd131433efe9d684
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-18 20:34:27 +0100

    pricing section finished

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -133,7 +133,7 @@ export const pricingOptions = [
   },
   {
     title: "Pro",
-    price: "$25",
+    price: "$45",
     features: [
       "Private board sharing",
       "10 Gb Storage",
@@ -143,7 +143,7 @@ export const pricingOptions = [
   },
   {
     title: "Enterprise",
-    price: "$65",
+    price: "$100",
     features: [
       "Private board sharing",
       "Unlimited Storage",
