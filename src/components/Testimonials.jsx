165b5d41f16de4eccc4ad5938f9c4810d00b2375
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-18 21:03:41 +0100

    testimonial section done

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -11,6 +11,18 @@ const Testimonials = () => {
                     <div key={index} className="w-full sm:1/2 lg:w-1/3 px-4 py-2">
                         <div className="bg-neutral rounded-md p-6 text-md border border-neutral-800 font-thin">
                             <p>{testimonial.text}</p>
+                            <div className="flex mt-8 items-center">
+                                <img className="w-12 h-12 mr-6 rounded-full border border-neutral-300" 
+                                src={testimonial.image} 
+                                alt={testimonial.user} 
+                                />
+                                <div>
+                                    <h6>{testimonial.user}</h6>
+                                    <span className="text-sm font-normal italic text-neutral-600">
+                                        {testimonial.company}
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 ))}
