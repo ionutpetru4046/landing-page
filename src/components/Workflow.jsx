afbf3b7d1026e2e2c9d5a081cce68b0fd7237bc3
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-18 19:13:53 +0100

    workflow component added and finished all changes

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -22,6 +22,10 @@ const Workflow = () => {
                         <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
                             <CheckCircle2 />
                         </div>
+                        <div>
+                            <h5 className="mt-1 mb-2 text-xl">{item.title}</h5>
+                            <p className="text-md text-neutral-500">{item.description}</p>
+                        </div>
                     </div>
                 ))}
             </div>
