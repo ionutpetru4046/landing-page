7204d4e3420f4f1f28065dbf4d61c22480677499
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-18 18:46:17 +0100

    workflow component added

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,7 +1,21 @@
+import { CheckCircle2 } from "lucide-react";
+import codeImg from "../assets/profile-pictures/code.jpg";
+
 const Workflow = () => {
   return (
-    <div>
-      
+    <div className="mt-20 ">
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
+        Accelerate your 
+        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+            {" "}
+            coding workflow.
+            </span>
+        </h2>
+        <div className="flex flex-wrap justify-center ">
+            <div className="p-2 w-full lg:w-1/2">
+                <img src={codeImg} alt="Code" />
+            </div>
+        </div>
     </div>
   )
 }
